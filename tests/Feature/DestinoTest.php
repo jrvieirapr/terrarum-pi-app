97dd3cf66637d21b96f96f50e9c408b034870fdd
IslaMCastro <133907365+IslaMCastro@users.noreply.github.com>
2023-09-22 16:46:25 -0300

    Alterações

--- tests/Feature/DestinoTest.php
+++ tests/Feature/DestinoTest.php
@@ -28,5 +28,73 @@ class DestinoTest extends TestCase
                 ]
             ]);
     }   
+
+   /**
+     * Criar um destino
+     */
+    public function testCriarDestinoSucesso(){
+
+        //Criar o objeto
+        $data = [
+            "nome" => $this->faker->word
+        ];
+
+        //Debug
+        //dd($data);
+
+        // Fazer uma requisição POST -  para salvar
+        $response = $this->postJson('/api/destinos', $data);
+
+        //dd($response);
+
+        // Verifique se teve um retorno 201 - Criado com Sucesso
+        // e se a estrutura do JSON Corresponde
+        $response->assertStatus(201)// preciso desse retorno
+            ->assertJsonStructure(['id', 'nome','coordenadas', 'created_at', 'updated_at']);// com essas informações
+
+    }
+
+    
+    /**
+     * Teste de criação com falhas
+     *
+     * @return void
+     */
+    public function testCriacaoDestinoFalha()
+    {
+        $data = [
+            "nome" => 'a'
+        ];
+         // Fazer uma requisição POST
+        $response = $this->postJson('/api/destinos', $data);
+
+        // Verifique se teve um retorno 422 - Falha no salvamento
+        // e se a estrutura do JSON Corresponde
+        $response->assertStatus(422)// se quero falha é essa resposta qie eu quero
+            ->assertJsonValidationErrors(['nome']); // validacao nesse campo
+    }
+
+     /**
+     * Teste de pesquisa de registro
+     *
+     * @return void
+     */
+    public function testPesquisadestinoSucesso()
+    {
+        // Criar um tipo
+        $destino = Destino::factory()->create();
+
+        
+        // Fazer pesquisa
+        $response = $this->getJson('/api/destinos/' . $destino->id);   
+        
+        // Verificar saida //no show(controller)
+        $response->assertStatus(200)
+            ->assertJson([
+                'id' => $destino->id,
+                'nome' => $destino->nome,
+                'coordenadas' => $destino->coordenadas,                
+            ]);
+    }
    
 }
