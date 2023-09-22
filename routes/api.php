7e8f5e9af142c14ff119279a926d9b4b7fd78425
JoaoVW06 <134637627+JoaoVW06@users.noreply.github.com>
2023-09-22 13:47:17 -0300

    Evento1.2(controller, store, update, evento route)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//rotas do evento
+Route::middleware('api')->prefix('eventos')->group(function(){
+    Route::get('/',[EventoController::class,'index']);
+    Route::post('/', [EventoController::class,'store']);
+    Route::get('/{evento}',[EventoController::class, 'show']);
+    Route::put('/{evento}',[EventoController::class, 'update']);
+    Route::delete('/{evento}',[EventoController::class, 'destroy']);
 });
