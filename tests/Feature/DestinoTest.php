845ce1983af235cbb311eda549f2b8dbeaaff2f8
HenriqueAkiraKagawa <japakagawa180@hotmail.com>
2023-09-27 15:06:06 -0300

    Alteracao2

--- tests/Feature/DestinoTest.php
+++ tests/Feature/DestinoTest.php
@@ -98,3 +98,4 @@ class DestinoTest extends TestCase
     }
    
 }
+
--- tests/Feature/ProdutoTest.php
+++ tests/Feature/ProdutoTest.php
@@ -2,19 +2,32 @@
 
 namespace Tests\Feature;
 
+use App\Models\Produto;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProdutoTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase, WithFaker;
 
-        $response->assertStatus(200);
-    }
+    public function testListarTodosProduto()
+    {
+        Produto::factory()->count(5)->create();
+
+        // usar metodo GET para verificar o retorno
+        $response = $this->getJson('/api/destinos');
+
+        //Testar ou verificar saida
+        $response->assertStatus(200)
+            ->assertJsonCount(5, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => ['id','descricao', 'destino_id', 'tipo_de_produto_id', 'esta_ativo', 'created_at', 'updated_at']
+
+                ]
+            ]);
+    }   
+
 }
+
